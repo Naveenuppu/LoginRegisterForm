f47c625f93c0e8e14d0b7ae6d7c0e2bccf94e919
Navin u <idiot@IDIOTs-MacBook-Pro.local>
2024-03-04 13:44:35 +0530

    Initial commit one

---
+++ src/CommonConstants.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+
+public class CommonConstants {
+    //This is for Frame configuration
+    public static final Dimension FRAME_SIZE = new Dimension(540,760);
+
+
+}
---
+++ src/LoginGui.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class LoginGui extends JFrame {
+    public LoginGui(){
+        super("Login Application!");
+        setSize(CommonConstants.FRAME_SIZE);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(null);
+
+        //login components
+        addGuiComponent();
+
+    }
+    private void addGuiComponent(){
+
+    }
+
+}
